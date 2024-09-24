355560436948bdc35e20cba7a64af98eb784d722
Jimmyale3102 <jimmyale3102@gmail.com>
2024-09-24 20:49:18 +1000

    feat: Add and display compose calendar library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,6 +59,8 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    // Compose Calendar implementation
+    implementation(libs.compose.calendar)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/dev/alejo/composecalendar/MainActivity.kt
+++ app/src/main/java/dev/alejo/composecalendar/MainActivity.kt
@@ -4,6 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.ui.Modifier
+import dev.alejo.compose_calendar.ComposeCalendar
 import dev.alejo.composecalendar.ui.theme.ComposeCalendarTheme
 
 class MainActivity : ComponentActivity() {
@@ -12,7 +17,10 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ComposeCalendarTheme {
-
+                Scaffold { innerPadding ->
+                    Text(modifier = Modifier.padding(innerPadding), text = "")
+                    ComposeCalendar()
+                }
             }
         }
     }
