190495f9d7899a6ef4e738e1571a9d39f2ec7831
Jimmyale3102 <jimmyale3102@gmail.com>
2024-09-24 19:28:09 +1000

    feat: Fix Maven configuration

--- app/src/main/java/dev/alejo/composecalendar/MainActivity.kt
+++ app/src/main/java/dev/alejo/composecalendar/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import dev.alejo.compose_calendar.ComposeCalendar
 import dev.alejo.composecalendar.ui.theme.ComposeCalendarTheme
 
 class MainActivity : ComponentActivity() {
@@ -13,7 +12,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ComposeCalendarTheme {
-                ComposeCalendar()
+
             }
         }
     }
--- compose_calendar/build.gradle.kts
+++ compose_calendar/build.gradle.kts
@@ -25,11 +25,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "11"
     }
 
     buildFeatures {
@@ -59,14 +59,10 @@ dependencies {
     androidTestImplementation(libs.androidx.espresso.core)
 }
 
-publishing {
-    publications {
-        register<MavenPublication>("release") {
-            groupId = "com.github.jimmyale3102"
-            artifactId = "compose_calendar"
-            version = "1.0.1"
-
-            afterEvaluate {
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("release") {
                 from(components["release"])
             }
         }
