fc6755c5259c35a0cf4216f06df52f2fc4f09e55
Jimmyale3102 <jimmyale3102@gmail.com>
2024-09-24 11:08:02 +1000

    feat: Add Navigation Button component

---
+++ compose_calendar/src/main/java/dev/alejo/compose_calendar/ui/component/NavigationButton.kt
@@ -0,0 +1,33 @@
+package dev.alejo.compose_calendar.ui.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import dev.alejo.compose_calendar.R
+import dev.alejo.compose_calendar.ui.utils.AppDimens
+import dev.alejo.compose_calendar.ui.utils.Blue10
+
+@Composable
+fun NavigationButton(
+    onClick: () -> Unit,
+    icon: ImageVector
+) {
+    IconButton(
+        modifier = Modifier
+            .background(Blue10, RoundedCornerShape(AppDimens.Small)),
+        onClick = { onClick() }
+    ) {
+        Icon(
+            imageVector = icon,
+            contentDescription = stringResource(id = R.string.navigation_button_content_description),
+            modifier = Modifier.offset(x = 4.dp)
+        )
+    }
+}
---
+++ compose_calendar/src/main/java/dev/alejo/compose_calendar/ui/utils/Color.kt
@@ -0,0 +1,5 @@
+package dev.alejo.compose_calendar.ui.utils
+
+import androidx.compose.ui.graphics.Color
+
+val Blue10 = Color(0x1A3E7DE8)
