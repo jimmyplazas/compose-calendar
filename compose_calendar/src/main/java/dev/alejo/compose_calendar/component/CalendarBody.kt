9fcacdfd510d52d11c544f11ee538060b5ef26ec
Jimmyale3102 <jimmyale3102@gmail.com>
2025-05-18 16:51:45 +0700

    feat: Add animation and dynamic day generation to CalendarBody

--- compose_calendar/src/main/java/dev/alejo/compose_calendar/component/CalendarBody.kt
+++ compose_calendar/src/main/java/dev/alejo/compose_calendar/component/CalendarBody.kt
@@ -1,5 +1,7 @@
 package dev.alejo.compose_calendar.component
 
+import android.annotation.SuppressLint
+import androidx.compose.animation.AnimatedContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -8,30 +10,58 @@ import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import dev.alejo.compose_calendar.CalendarEvent
 import dev.alejo.compose_calendar.util.CalendarColors
 import dev.alejo.compose_calendar.util.CalendarDefaults
-import dev.alejo.compose_calendar.util.CalendarDefaults.calendarColors
-import java.time.LocalDate
+import dev.alejo.compose_calendar.util.daysOfTheMonth
+import kotlinx.datetime.LocalDate
+import kotlinx.datetime.isoDayNumber
 
+@SuppressLint("UnusedContentLambdaTargetStateParameter")
 @Composable
 fun CalendarBody(
-    date: LocalDate,
+    currentDate: LocalDate,
+    events: List<CalendarEvent>,
+    onDayClick: (CalendarEvent?) -> Unit = {},
+    calendarColors: CalendarColors,
+    animatedBody: Boolean
+) {
+    val date = LocalDate(currentDate.year, currentDate.monthNumber, 1)
+
+    /**
+     * List of days including leading empty days
+     *
+     * The first list is the leading empty days. Which is the number of the dayOfWeek
+     * minus 1, for example:
+     * If the first day of the week is 3, it means it is Wednesday, then it must be 3 - 1 which is
+     * Tuesday, so as a result it is: [Monday: null, Tuesday: null, Wednesday: 1, Thursday:2, ...]
+     *
+     * The second list is the days of the month
+     */
+    val leadingEmptyDays = date.dayOfWeek.isoDayNumber - 1
+    val days = List(leadingEmptyDays) { null } + List(currentDate.daysOfTheMonth()) { it + 1 }
+
+    if (animatedBody) {
+        AnimatedContent(days) {
+            BodyContent(days, currentDate, events, onDayClick, calendarColors)
+        }
+    } else {
+        BodyContent(days, currentDate, events, onDayClick, calendarColors)
+    }
+}
+
+@Composable
+private fun BodyContent(
+    days: List<Int?>,
+    currentDate: LocalDate,
     events: List<CalendarEvent>,
     onDayClick: (CalendarEvent?) -> Unit = {},
     calendarColors: CalendarColors
 ) {
-    val startDay = date.withDayOfMonth(1)
-    val daysInMonth = startDay.lengthOfMonth()
-    val startDayOfWeek = (startDay.dayOfWeek.value - 1) % 7
-
-    // List of days including leading empty days
-    val days = List(startDayOfWeek) { "" } + List(daysInMonth) { it + 1 }
-
     LazyVerticalGrid(
         columns = GridCells.Fixed(7),
-        modifier = Modifier.fillMaxWidth()
+        modifier = Modifier
+            .fillMaxWidth()
             .background(calendarColors.backgroundColor),
         horizontalArrangement = Arrangement.spacedBy(CalendarDefaults.Dimens.XSmall),
         verticalArrangement = Arrangement.spacedBy(CalendarDefaults.Dimens.XSmall)
@@ -40,16 +70,10 @@ fun CalendarBody(
             CalendarDay(
                 day = day,
                 events = events,
-                currentDate = date,
+                currentDate = currentDate,
                 onDayClick = { event -> onDayClick(event) },
                 calendarColors = calendarColors
             )
         }
     }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun CalendarPreview() {
-    CalendarBody(date = LocalDate.now(), events = emptyList(), calendarColors = calendarColors())
 }
