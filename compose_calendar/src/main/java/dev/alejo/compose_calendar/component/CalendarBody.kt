02f58c37a1323b8a1277be907901c08d0ccee08f
Jimmyale3102 <jimmyale3102@gmail.com>
2025-05-21 14:54:05 +0700

    feat: Refactor `CalendarBody` to use `java.time.LocalDate` and support customizable first day of week

--- compose_calendar/src/main/java/dev/alejo/compose_calendar/component/CalendarBody.kt
+++ compose_calendar/src/main/java/dev/alejo/compose_calendar/component/CalendarBody.kt
@@ -1,61 +1,71 @@
 package dev.alejo.compose_calendar.component
 
-import android.annotation.SuppressLint
-import androidx.compose.animation.AnimatedContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.grid.itemsIndexed
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import dev.alejo.compose_calendar.CalendarEvent
 import dev.alejo.compose_calendar.util.CalendarColors
 import dev.alejo.compose_calendar.util.CalendarDefaults
-import dev.alejo.compose_calendar.util.daysOfTheMonth
-import kotlinx.datetime.LocalDate
-import kotlinx.datetime.isoDayNumber
+import java.time.DayOfWeek
+import java.time.LocalDate
 
-@SuppressLint("UnusedContentLambdaTargetStateParameter")
+/**
+ * Composable function that represents the body of a calendar.
+ * It displays the days of a given month and allows interaction with them.
+ *
+ * @param <T> The type of the events in the calendar.
+ * @param currentDate The current date to display the month for.
+ * @param events A map of dates to lists of calendar events.
+ * @param onDayClick A lambda function to be invoked when a day is clicked. It receives the clicked date and the list of events for that day.
+ * @param calendarColors The colors to be used for styling the calendar.
+ * @param isContentClickable A boolean indicating whether the content of the days is clickable.
+ * @param firstDayOfWeek The first day of the week to be displayed in the calendar.
+ * @see CalendarEvent
+ */
 @Composable
-fun CalendarBody(
+fun <T> CalendarBody(
     currentDate: LocalDate,
-    events: List<CalendarEvent>,
-    onDayClick: (LocalDate, List<CalendarEvent>) -> Unit = { _, _ -> },
+    events: Map<LocalDate, List<CalendarEvent<T>>>,
+    onDayClick: (LocalDate, List<CalendarEvent<T>>) -> Unit = { _, _ -> },
     calendarColors: CalendarColors,
-    animatedBody: Boolean
+    isContentClickable: Boolean,
+    firstDayOfWeek: DayOfWeek
 ) {
-    val date = LocalDate(currentDate.year, currentDate.monthNumber, 1)
+    val days = remember(currentDate, firstDayOfWeek) {
+        val firstDateOfMonth = currentDate.withDayOfMonth(1)
+        val dayOfWeekOfFirstDayMonth = firstDateOfMonth.dayOfWeek
+        val leadingEmptyDays = ((dayOfWeekOfFirstDayMonth.value - firstDayOfWeek.value + 7) % 7)
 
-    /**
-     * List of days including leading empty days
-     *
-     * The first list is the leading empty days. Which is the number of the dayOfWeek
-     * minus 1, for example:
-     * If the first day of the week is 3, it means it is Wednesday, then it must be 3 - 1 which is
-     * Tuesday, so as a result it is: [Monday: null, Tuesday: null, Wednesday: 1, Thursday:2, ...]
-     *
-     * The second list is the days of the month
-     */
-    val leadingEmptyDays = date.dayOfWeek.isoDayNumber - 1
-    val days = List(leadingEmptyDays) { null } + List(currentDate.daysOfTheMonth()) { it + 1 }
-
-    if (animatedBody) {
-        AnimatedContent(days) {
-            BodyContent(days, currentDate, events, onDayClick, calendarColors)
+        List(leadingEmptyDays) { null } + List(currentDate.lengthOfMonth()) {
+            LocalDate.of(currentDate.year, currentDate.monthValue, it + 1)
         }
-    } else {
-        BodyContent(days, currentDate, events, onDayClick, calendarColors)
     }
+
+    BodyContent(days, events, isContentClickable, onDayClick, calendarColors)
 }
 
+/**
+ * Composable function that represents the content of the calendar body.
+ * It displays the days in a grid layout.
+ *
+ * @param <T> The type of the events in the calendar.
+ * @param days The list of dates to display in the calendar. Null values represent empty cells.
+ * @param events A map of dates to lists of calendar events.
+ * @param isContentClickable A boolean indicating whether the content of the days is clickable.
+ * @param onDayClick A lambda function to be invoked when a day is clicked.
+ */
 @Composable
-private fun BodyContent(
-    days: List<Int?>,
-    currentDate: LocalDate,
-    events: List<CalendarEvent>,
-    onDayClick: (LocalDate, List<CalendarEvent>) -> Unit = { _, _ -> },
+private fun <T> BodyContent(
+    days: List<LocalDate?>,
+    events: Map<LocalDate, List<CalendarEvent<T>>>,
+    isContentClickable: Boolean,
+    onDayClick: (LocalDate, List<CalendarEvent<T>>) -> Unit = { _, _ -> },
     calendarColors: CalendarColors
 ) {
     LazyVerticalGrid(
@@ -66,11 +76,14 @@ private fun BodyContent(
         horizontalArrangement = Arrangement.spacedBy(CalendarDefaults.Dimens.XSmall),
         verticalArrangement = Arrangement.spacedBy(CalendarDefaults.Dimens.XSmall)
     ) {
-        items(days) { day ->
+        itemsIndexed(
+            items = days,
+            key = { index: Int, dayData: LocalDate? -> dayData?.toString() ?: "emptyDay-$index" }
+        ) { _, day ->
             CalendarDay(
                 day = day,
-                events = events,
-                currentDate = currentDate,
+                events = events[day] ?: emptyList(),
+                isContentClickable = isContentClickable,
                 onDayClick = { date, events -> onDayClick(date, events) },
                 calendarColors = calendarColors
             )
