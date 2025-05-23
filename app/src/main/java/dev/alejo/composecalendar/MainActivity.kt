89cb33537e57408cab6d2cfbc2c6bea3efc03859
Jimmyale3102 <jimmyale3102@gmail.com>
2025-05-23 16:18:53 +0700

    feat: Add `SimpleComposeCalendar` component

--- app/src/main/java/dev/alejo/composecalendar/MainActivity.kt
+++ app/src/main/java/dev/alejo/composecalendar/MainActivity.kt
@@ -7,12 +7,17 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -23,43 +28,63 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import dev.alejo.compose_calendar.CalendarEvent
 import dev.alejo.compose_calendar.ComposeCalendar
-import dev.alejo.compose_calendar.util.CalendarDefaults.calendarColors
+import dev.alejo.compose_calendar.SimpleComposeCalendar
+import dev.alejo.compose_calendar.util.CalendarDefaults
 import dev.alejo.composecalendar.ui.theme.ComposeCalendarTheme
+import java.time.LocalDate
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        val viewModel = MainViewModel()
         setContent {
+            val state by viewModel.state.collectAsState()
             ComposeCalendarTheme {
-                Calendar()
+                Scaffold { innerPadding ->
+                    Calendar(Modifier.padding(innerPadding), state)
+                }
             }
         }
     }
 }
 
 @Composable
-fun Calendar() {
-    Column(modifier = Modifier.padding(16.dp), horizontalAlignment = Alignment.CenterHorizontally) {
-        Text(
-            text = "Compose Calendar",
-            fontSize = 32.sp,
-            fontWeight = FontWeight.Bold,
-            color = MaterialTheme.colorScheme.primary
-        )
-        ComposeCalendar(
+fun Calendar(modifier: Modifier = Modifier, state: CalendarState) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(start = 16.dp, end = 16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        SimpleComposeCalendar(
             modifier = Modifier.padding(16.dp),
-            events = getListOfEvents(),
-            calendarColors = calendarColors(
-                eventBackgroundColor = MaterialTheme.colorScheme.surfaceContainerLow,
-                eventContentColor = MaterialTheme.colorScheme.onSurface,
-                backgroundColor = Color.White,
-                headerBackgroundColor = Color.White
-            ),
+            events = state.simpleEvents,
             onDayClick = { date, event ->
                 println(date.toString())
                 println(event.toString())
             },
+            eventIndicator = { _, _, _ ->
+                IndicatorContent(Modifier.size(8.dp))
+            }
+        )
+        ComposeCalendar(
+            events = state.events,
+            onDayClick = { _, _ -> },
+            eventIndicator = { _, position, events ->
+                if (position < 2) {
+                    IndicatorContent(Modifier.fillMaxWidth().height(8.dp))
+                }
+                if (position == 2) {
+                    Text(
+                        text = "+${events - 2}",
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 10.sp
+                    )
+                }
+            },
+            maxIndicators = CalendarDefaults.IndicatorLimit.Three,
+            indicatorLayout = CalendarDefaults.IndicatorLayout.Column,
             isContentClickable = false,
             onPreviousMonthClick = { println("Prev") },
             onNextMonthClick = { println("Next") }
@@ -70,40 +95,60 @@ fun Calendar() {
 private fun getListOfEvents() = listOf(
     CalendarEvent(
         data = MyData("Event 1"),
-        date = java.time.LocalDate.now().plusDays(1),
-        indicatorContent = { IndicatorContent() }
+        date = LocalDate.now()
+    ),
+    CalendarEvent(
+        data = MyData("Event 1"),
+        date = LocalDate.now().minusDays(20)
     ),
     CalendarEvent(
         data = MyData("Event 2"),
-        date = java.time.LocalDate.now().plusDays(1),
-        indicatorContent = { IndicatorContent(Color.Red) }
+        date = LocalDate.now().minusDays(20)
+    ),
+    CalendarEvent(
+        data = MyData("Event 1"),
+        date = LocalDate.now().plusDays(1)
+    ),
+    CalendarEvent(
+        data = MyData("Event 2"),
+        date = LocalDate.now().plusDays(1)
     ),
     CalendarEvent(
         data = MyData("Event 3"),
-        date = java.time.LocalDate.now().plusDays(1),
-        indicatorContent = { IndicatorContent() }
+        date = LocalDate.now().plusDays(1)
     ),
     CalendarEvent(
         data = MyData("Event 4"),
-        date = java.time.LocalDate.now().plusDays(1),
-        indicatorContent = { IndicatorContent() }
+        date = LocalDate.now().plusDays(1)
+    ),
+    CalendarEvent(
+        data = MyData("Event 3"),
+        date = LocalDate.now().plusDays(4)
     ),
     CalendarEvent(
         data = MyData("Event 4"),
-        date = java.time.LocalDate.now().plusDays(5),
-        indicatorContent = { IndicatorContent() }
+        date = LocalDate.now().plusDays(4)
+    ),
+    CalendarEvent(
+        data = MyData("Event 4"),
+        date = LocalDate.now().plusDays(4)
     ),
 )
 
 data class MyData(val name: String)
 
 @Composable
-fun IndicatorContent(color: Color = Color.Black) {
-    Box(Modifier.size(8.dp).clip(CircleShape).background(color))
+fun IndicatorContent(modifier: Modifier = Modifier, color: Color = Color.Black) {
+    Box(modifier.clip(CircleShape).background(color))
 }
 
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun ComposeCalendarPreview() {
-    Calendar()
+    Calendar(
+        state = CalendarState(
+            events = getListOfEvents(),
+            simpleEvents = getListOfEvents().map { it.date }
+        )
+    )
 }
