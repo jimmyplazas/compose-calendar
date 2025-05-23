3aeda436475dcc4be660556f757c2b828f00b389
Jimmyale3102 <jimmyale3102@gmail.com>
2025-05-23 16:19:39 +0700

    Refactor: Remove indicatorContent from CalendarEvent

--- compose_calendar/src/main/java/dev/alejo/compose_calendar/CalendarEvent.kt
+++ compose_calendar/src/main/java/dev/alejo/compose_calendar/CalendarEvent.kt
@@ -1,6 +1,5 @@
 package dev.alejo.compose_calendar
 
-import androidx.compose.runtime.Composable
 import java.time.LocalDate
 
 /**
@@ -9,10 +8,8 @@ import java.time.LocalDate
  * @param T Tipo genérico que representa los datos asociados al evento.
  * @property data Información adicional opcional relacionada con el evento.
  * @property date Fecha del evento.
- * @property indicatorContent Composable opcional que representa un icono o indicador visual para el evento.
  */
 data class CalendarEvent<T>(
     val data: T? = null,
-    val date: LocalDate,
-    val indicatorContent: (@Composable () -> Unit)? = null
+    val date: LocalDate
 )
